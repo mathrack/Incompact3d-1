39d33277f0747fee46a18d8b7b80d8af96025cc8
cflag <cedric.flageul@univ-poitiers.fr>
2022-10-07 17:02:07 +0200

    Remove bash script

--- run_cavity_2d/minmax.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-more nohup.out | grep U,V,W | grep min | cut -c12-43 > uvmin
-more nohup.out | grep U,V,W | grep max | cut -c12-43 > uvmax
-more nohup.out | grep Phi1 | cut -c15- > phiminmax
