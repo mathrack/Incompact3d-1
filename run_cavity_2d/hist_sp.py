b5f4e79c9f9b9d18955c7af90bbecc164235232a
cflag <cedric.flageul@univ-poitiers.fr>
2022-10-09 10:27:17 +0200

    Add a python script to extract and plot the histogram a a location, single precision run only.

---
+++ run_cavity_2d/hist_sp.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+# Subroutine to extract / plot the histogram of a variable
+def extract_and_plot(f, name):
+    # Matplotlib figure and axis
+    if (plot):
+        fig, ax = plt.subplots()
+    #
+    # Size of the histogram
+    tmp = f.read(4)
+    n = np.fromfile(f, dtype=np.int32, count=1)
+    tmp = f.read(4)
+    #
+    # First bin
+    tmp = f.read(4)
+    xmin = np.fromfile(f, dtype=np.float32, count=1)
+    xmax = np.fromfile(f, dtype=np.float32, count=1)
+    xnumr = np.fromfile(f, dtype=np.int32, count=1)
+    np.fromfile(f, dtype=np.float32, count=1)
+    tmp = f.read(4)
+    if (plot):
+        xmin = xmin[0]
+        xmax = xmax[0]
+        xnumr = max(xnumr[0], 0.5)
+        if xnumr>=1:
+            tmp = ax.add_patch(Rectangle((xmin, 0.), xmax-xmin, float(xnumr)))
+        if xnumr<0:
+            print("Error in first bin")
+        tmp = ax.set_xlim(left=xmin)
+    #
+    # Read the size bins
+    for ibin in range(n[0]-2):
+        tmp = f.read(4)
+        xmin = np.fromfile(f, dtype=np.float32, count=1)
+        xmax = np.fromfile(f, dtype=np.float32, count=1)
+        xnum = np.fromfile(f, dtype=np.int32, count=1)
+        tmp = f.read(4)
+        if (plot):
+            xmin = xmin[0]
+            xmax = xmax[0]
+            xnum = xnum[0]
+            xnumr = max(xnum, xnumr)
+            if xnum>0:
+                tmp = ax.add_patch(Rectangle((xmin, 0.), xmax-xmin, float(xnum)))
+                tmp = ax.set_ylim(top=xnumr)
+            if xnum<0:
+                print("Error in bin "+str(ibin))
+    #
+    # Last bin
+    tmp = f.read(4)
+    xmin = np.fromfile(f, dtype=np.float32, count=1)
+    xmax = np.fromfile(f, dtype=np.float32, count=1)
+    xnum = np.fromfile(f, dtype=np.int32, count=1)
+    np.fromfile(f, dtype=np.float32, count=1)
+    tmp = f.read(4)
+    if (plot):
+        xmin = xmin[0]
+        xmax = xmax[0]
+        xnum = xnum[0]
+        xnumr = max(xnum, xnumr)
+        if xnum>0:
+            tmp = ax.add_patch(Rectangle((xmin, 0.), xmax-xmin, float(xnum)))
+            tmp = ax.set_ylim(top=xnumr)
+        if xnum<0:
+            print("Error in last bin")
+        tmp = ax.set_xlim(right=xmax)
+    #
+    # Display
+    if (plot):
+        tmp = ax.set_ylim(bottom=1.)
+        tmp = ax.set_xlabel("Value")
+        tmp = ax.set_ylabel("Number of samples")
+        tmp = ax.set_yscale("log")
+        tmp = ax.set_title("Histogram for "+name)
+        tmp = plt.show()
+
+# Flag to activate the plot
+plot = True
+
+# Name of the file to process
+fname = "out/histogram_100_120.bin"
+
+# Open the file
+f = open(fname, "rb")
+
+extract_and_plot(f, "u")
+extract_and_plot(f, "v")
+extract_and_plot(f, "t")
+
+extract_and_plot(f, "dudx")
+extract_and_plot(f, "dvdx")
+extract_and_plot(f, "dtdx")
+
+extract_and_plot(f, "dudy")
+extract_and_plot(f, "dvdy")
+extract_and_plot(f, "dtdy")
+
+# Close the file
+f.close()
